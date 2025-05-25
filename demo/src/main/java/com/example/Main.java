2ae72870720c1246da60b5277692ce65dad67c2c
Nishant Yadav <inishantyadav24@gmail.com>
2025-05-25 23:32:50 +0530

    Bean scopes

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -5,9 +5,17 @@ import com.example.dummy.Alien;
 
 public class Main {
 	public static void main(String[] args) {
-		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
-			Alien alien = context.getBean("alien", Alien.class);
-			alien.code();
-		}
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+		Alien alien1 = context.getBean("alien", Alien.class);
+		alien1.setAge(10);
+		alien1.code();
+		System.out.println(alien1.getAge());
+
+		Alien alien2 = context.getBean("alien", Alien.class);
+		alien2.code();
+		System.out.println(alien2.getAge());
+
+		((ClassPathXmlApplicationContext) context).close();
 	}
 }
--- demo/src/main/java/com/example/dummy/Alien.java
+++ demo/src/main/java/com/example/dummy/Alien.java
@@ -1,7 +1,15 @@
 package com.example.dummy;
 
 public class Alien {
-  Laptop laptop;
+  int age;
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
 
   public void code() {
     System.out.println("Coding...");
