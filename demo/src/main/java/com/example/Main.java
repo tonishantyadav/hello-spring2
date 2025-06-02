16542aa37ad71e69b51df746f73e0836830a50d1
Nishant Yadav <inishantyadav24@gmail.com>
2025-06-02 23:19:32 +0530

    Autowiring

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,13 +3,15 @@ package com.example;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.example.config.AppConfig;
-import com.example.dummy.Desktop;
+import com.example.dummy.Alien;
 
 public class Main {
 	public static void main(String[] args) {
 		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)) {
-			Desktop desktop = context.getBean(Desktop.class);
-			desktop.compile();
+			Alien alien = context.getBean(Alien.class);
+			System.out.println(alien.getAge());
+			System.out.println(alien.getSalary());
+			alien.code();
 		}
 	}
 }
--- demo/src/main/java/com/example/config/AppConfig.java
+++ demo/src/main/java/com/example/config/AppConfig.java
@@ -3,10 +3,21 @@ package com.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.dummy.Alien;
+import com.example.dummy.Computer;
 import com.example.dummy.Desktop;
 
 @Configuration
 public class AppConfig {
+  @Bean
+  public Alien alien(Computer com) {
+    Alien alien = new Alien();
+    alien.setAge(10);
+    alien.setSalary(1000);
+    alien.setComputer(com);
+    return alien;
+  }
+
   @Bean
   public Desktop desktop() {
     return new Desktop();
