65735425ab013528f0c0b5eae8447639f1e4eb86
Nishant Yadav <inishantyadav24@gmail.com>
2025-06-02 23:23:09 +0530

    Primary and Qualifier

--- demo/src/main/java/com/example/config/AppConfig.java
+++ demo/src/main/java/com/example/config/AppConfig.java
@@ -1,16 +1,19 @@
 package com.example.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.example.dummy.Alien;
 import com.example.dummy.Computer;
 import com.example.dummy.Desktop;
+import com.example.dummy.Laptop;
 
 @Configuration
 public class AppConfig {
   @Bean
-  public Alien alien(Computer com) {
+  public Alien alien(Computer com) { // @Qualifier("laptop")
     Alien alien = new Alien();
     alien.setAge(10);
     alien.setSalary(1000);
@@ -22,4 +25,11 @@ public class AppConfig {
   public Desktop desktop() {
     return new Desktop();
   }
+
+  @Bean
+  @Primary
+  public Laptop laptop() {
+    return new Laptop();
+  }
+
 }
