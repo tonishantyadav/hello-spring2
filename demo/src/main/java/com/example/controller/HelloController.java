3e120bd8ec9a1dce95793707c55770ecd14b0b94
tonishantyadav <inishantyadav24@gmail.com>
2025-07-27 09:14:02 +0530

    Refactor pom.xml; remove dotenv dependency and clean up project structure. Add HelloController with /add endpoint for addition functionality and create result.jsp for displaying results. Update index.jsp for consistency and add style.css for improved styling.

--- demo/src/main/java/com/example/controller/HelloController.java
+++ demo/src/main/java/com/example/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
@@ -9,5 +12,17 @@ public class HelloController {
   public String hello() {
     System.out.println("Controller: /hello called");
     return "index.jsp";
+  }
+  
+  @RequestMapping("add")
+  public String add(HttpServletRequest req) {
+    System.out.println("Controller: /add called");
+    
+    int num1 = Integer.parseInt(req.getParameter("num1"));
+    int num2 = Integer.parseInt(req.getParameter("num2"));
+    
+    int result = num1 + num2;
+    System.out.println("Result: " + result);
+    return "result.jsp";
   }
 }
